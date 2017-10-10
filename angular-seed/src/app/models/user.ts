d923424b8d7d9704463bb15b0589a89193994e38
Edwin Ceron <edwin.ceron@hotmail.com>
2017-10-10 15:37:34 -0500

    user_pasword fixed

--- angular-seed/src/app/models/user.ts
+++ angular-seed/src/app/models/user.ts
@@ -4,7 +4,7 @@ import { Order } from "./order";
 export class User {
     public id : number;
     private email: string;
-    private password: string;
+    private user_password: string;
     public firstname: string;
     private lastname: string;
     private city: string;
@@ -16,7 +16,7 @@ export class User {
 
     constructor(email: string, password: string, firstname: string, lastname: string, image: string, city: string, cellphone: string) {
         this.email = email;
-        this.password = password;
+        this.user_password = password;
         this.firstname = firstname;
         this.lastname = lastname;
         this.image = image;
